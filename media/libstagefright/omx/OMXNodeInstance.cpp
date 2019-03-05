7472476df2487d369b90e0da8860d1d8a198efca
phenomenall <venkysaihot@gmail.com>
2019-03-05 13:51:07 +0100

    Revert "OMXNodeInstance: Allow dynamic native handle mode for input buffers"
    This reverts commit bda339098d273066fe50d4cb77503bac5ea4ede0.

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -1077,8 +1077,7 @@ status_t OMXNodeInstance::useBuffer(
         }
 
         case OMXBuffer::kBufferTypeSharedMem: {
-            if (mPortMode[portIndex] != IOMX::kPortModePresetByteBuffer
-                    && mPortMode[portIndex] != IOMX::kPortModeDynamicANWBuffer) {
+            if (mPortMode[portIndex] != IOMX::kPortModePresetByteBuffer) {
                 break;
             }
             return useBuffer_l(portIndex, omxBuffer.mMem, NULL, buffer);
@@ -1094,8 +1093,7 @@ status_t OMXNodeInstance::useBuffer(
 
         case OMXBuffer::kBufferTypeHidlMemory: {
                 if (mPortMode[portIndex] != IOMX::kPortModePresetByteBuffer
-                        && mPortMode[portIndex] != IOMX::kPortModeDynamicANWBuffer
-                        && mPortMode[portIndex] != IOMX::kPortModeDynamicNativeHandle) {
+                        && mPortMode[portIndex] != IOMX::kPortModeDynamicANWBuffer) {
                     break;
                 }
                 sp<IHidlMemory> hidlMemory = mapMemory(omxBuffer.mHidlMemory);
@@ -1110,8 +1108,7 @@ status_t OMXNodeInstance::useBuffer(
             break;
     }
 
-    ALOGE("b/77486542 : bufferType = %d vs. portMode = %d",
-          omxBuffer.mBufferType, mPortMode[portIndex]);
+    ALOGE("b/77486542");
     android_errorWriteLog(0x534e4554, "77486542");
     return INVALID_OPERATION;
 }
